8b4ef02fc5276c04da192ce0a39f1ece61e4f53e
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-05-07 22:20:53 -0700

    t

---
+++ src/MinIntHeap.java
@@ -0,0 +1,180 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MinIntHeap {
+	private int[] items;
+	private int lastNode;
+	private int size;
+	 //constructor that sets max capacity of the heap to m
+	   public MinIntHeap( int m) 
+	   {  
+		   this.items = new int[m];
+		   this.lastNode = 0;;
+	   }   
+
+	//constructor that sets max capacity of heap to m; initializes the heap with the b values. 
+	//Use a private method called buildheap
+	   public MinIntHeap( int[] b, int m) 
+	   { 
+		   this.items = new int[m];
+		   
+		   buildHeap(b);
+	   }
+
+
+	//-------------- public methods -------------------------------
+	   public boolean isEmpty() 
+	   { 
+		   if(this.size == 0)
+			   return true;
+		   else
+			   return false;
+	   }
+	   public int size() 
+	   { 
+		   return this.size;
+	   }  //return number of values in the heap
+	   public void heapInsert(int v) 
+	   { 
+		   this.lastNode++;
+		   items[lastNode] = v;
+		   bubbleUp(lastNode);
+		   this.size++;
+		   
+	   }
+	   
+	   public int  removeMin () 
+	   { 
+		   int minValue = items[1];
+		   items[1] = items[lastNode];
+		   this.lastNode--;
+		   this.size--;
+		   bubbleDown(1);
+		   return minValue;
+	   }
+	   public int min() 
+	   {
+		   return items[1];
+	   }
+
+	 // Print the heap values in  heap array order
+	  public void printHeapValues() 
+	  {
+		  for(int i = 1; i <= this.size; i++)
+			  System.out.print(items[i] + " ");
+	  }
+	 
+	// Return a copy of the heap values in heap array order.
+	 public int[]  getHeapValues() 
+	 { 
+		 int[] z = new int[size];
+		 for(int i = 0; i < z.length; i++) 
+		 {
+			z[i] = items[i+1];
+		 }
+		 return z;
+	 }   
+
+	 //Uses heapsort algorithm to sort the array b. This is the only static method.
+	public static void heapsort( int[] b) 
+	{ 
+	
+	} 
+
+	  //------------private methods ---- including bubbleup, bubbledown and buildheap-------
+	private void buildHeap(int[] array) 
+	{
+		int k = 1;
+		this.lastNode = array.length;
+		for(int i = 0; i < array.length; i++) 
+		{
+			this.items[k] = array[i];
+			k++;
+			this.size++;
+		}
+		for(int j = (lastNode/2); j >= 1; j--)
+		{
+			bubbleDown(j);
+		}
+	}
+	
+	private void bubbleDown(int n) 
+	{
+		while(hasChild(n))
+		{
+			int child;
+			if(leftChild(n) == -1)
+				child = rightChild(n);
+			else if(rightChild(n) == -1)
+				child = leftChild(n);
+			else if(items[leftChild(n)] > items[rightChild(n)])
+				child = rightChild(n);
+			else
+				child = leftChild(n);
+			if(items[n] > items[child]) 
+			{
+				swap(n, child);
+				n = child;
+			}
+			else
+				break;
+			
+		}
+	}
+	
+	private void bubbleUp(int n) 
+	{
+		while(n != 1) 
+		{
+			if(items[n] < items[parent(n)]) 
+			{
+				swap(n,parent(n));
+				n = parent(n);
+			}
+			else
+				return;
+		}
+	}
+	
+	private boolean hasChild(int k) 
+	{
+		if((leftChild(k) != -1) || (rightChild(k) != -1)) 
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	private int leftChild(int k) 
+	{	
+		int left;
+		if((2*k) > items.length) 
+			left = -1;
+		else
+			left = (items[(2*k)] != 0) ? (2*k) : -1;
+		return left;
+	}
+	
+	private int rightChild(int k) 
+	{
+		int right;
+		if(((2*k)+1) > items.length)
+			right = -1;
+		else
+			right = (items[(2*k)+1] != 0) ? ((2*k)+1) : -1;
+		return right;
+	}
+	private int parent(int k) 
+	{
+		return (k/2);
+	}
+	private void swap(int a, int b) 
+	{
+		int tmp = items[b];
+		items[b] = items[a];
+		items[a] = tmp;
+		
+	}
+
+}
---
+++ src/Proj6InstructorsTestCase.java
@@ -0,0 +1,113 @@
+//Comp 333 Project 6
+//Instructor's Test Case
+
+import java.util.*;
+public class Proj6InstructorsTestCase
+{
+   public static void main(String[] args)
+   {
+   //Instructtor's Test Case
+      
+      System.out.println("\n----------------Testing public methods using first constructor------");
+      MinIntHeap h = new MinIntHeap(20);
+      int[] testValues =  {19, 35, 14, 45, 3, 66, 64, 44, 96, 97, 3,7, 12, 50 };
+      for(int k = 0; k < testValues.length; k++)
+         h.heapInsert(testValues[k]); 
+      System.out.print("\nPrint Heap Values:\n");
+      h.printHeapValues();
+      
+
+      for (int i = 0; i < 5; i++)
+         System.out.println( "removed min = " + h.removeMin());  
+   
+      h.heapInsert(50);  
+      System.out.println("heap size = " + h.size() + "\t\tisEmpty? " + h.isEmpty());
+      System.out.print("\nPrint Heap Values:\n");
+      h.printHeapValues();
+      int[] z = h.getHeapValues();
+      System.out.println("Get Heap Values\n" + Arrays.toString(z));
+      
+     
+      System.out.println("\n--------------Testing public methods using second constructor------");
+      int[] a = { 23,3,17,11,5,13,7,19};  
+      MinIntHeap h2 = new MinIntHeap(a, 20);
+      System.out.print("\nPrint Heap Values:\n");
+      h2.printHeapValues();
+//      h2.heapInsert(2);
+//      System.out.println( "removed min = " + h2.removeMin());
+//      System.out.println( "removed min = " + h2.removeMin());
+//      System.out.print("\nPrint Heap Values:\n");
+//      h2.printHeapValues();
+      
+   
+//      System.out.println("\n-------------Testing heapsort---------------------------------------");
+//      int[]  b = {-4, 6,7 ,-20, 50 ,70};
+//      System.out.println("unsorted array = " + Arrays.toString(b));
+//      MinIntHeap.heapSort(b);
+//      System.out.println("sorted array = " + Arrays.toString(b));
+//      
+//      System.out.println("\n----------------Testing Error messages-------------");
+//      MinIntHeap h3 = new MinIntHeap(5);
+//      for( int k = 1; k <= 6; k++)
+//          h3.heapInsert(2- (k-3)*(k -3));
+//      System.out.println("Print Heap Values");
+//      h3.printHeapValues();
+//      while( !h3.isEmpty())
+//         System.out.println( "removed min: " + h3.removeMin() + "  " );
+//      h3.removeMin();
+      
+      
+   }
+
+
+
+
+}
+/* -----------Testcase Answers--------------------
+
+
+----------------Testing public methods using first constructor------
+
+Print Heap Values:
+3  3  7  44  14  12  50  45  96  97  
+35  66  19  64  
+removed min = 3
+removed min = 3
+removed min = 7
+removed min = 12
+removed min = 14
+heap size = 10		isEmpty? false
+
+Print Heap Values:
+19  35  50  44  50  66  97  45  96  64  
+
+Get Heap Values
+[19, 35, 50, 44, 50, 66, 97, 45, 96, 64]
+
+--------------Testing public methods using second constructor------
+
+Print Heap Values:
+3  5  7  11  23  13  17  19  
+removed min = 2
+removed min = 3
+
+Print Heap Values:
+5  11  7  19  23  13  17  
+
+-------------Testing heapsort---------------------------------------
+unsorted array = [-4, 6, 7, -20, 50, 70]
+sorted array = [-20, -4, 6, 7, 50, 70]
+
+----------------Testing Error messages-------------
+Heap is full. Value Not Added = -7
+Print Heap Values
+-2  -2  2  1  1  
+removed min: -2  
+removed min: -2  
+removed min: 1  
+removed min: 1  
+removed min: 2  
+Cannot delete min from an empty heap
+
+
+*/
